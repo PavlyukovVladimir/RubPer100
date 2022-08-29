aa6228798741950453b2e746896a2d8ad850e8ad
Vladimir <vladimir.pavlyukov@thrive.io>
2022-08-29 19:35:43 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
 public class Main {
+
+    public static void main(String[] args) {
+        int initialAccountSum = 100;
+        int replenishment = 10001;
+        int minReplenishmentForBonuses = 1000;
+        int bonusCost = 100;
+
+        int bonusAmount = replenishment > minReplenishmentForBonuses ? replenishment / bonusCost : 0;
+        int totalAmount = initialAccountSum + replenishment + bonusAmount;
+
+        System.out.println("Total balance: " + totalAmount + "₽");
+        System.out.println(bonusAmount + "₽ " + "bonuses included");
+    }
+
 }
